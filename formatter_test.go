e72abfc4d592ae36b230662c7430b316aa148310
Fuwn <contact@fuwn.me>
2026-01-31 17:40:21 +0000

    refactor: Merge benchmarks into formatter_test.go

--- formatter_test.go
+++ formatter_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"strings"
 	"testing"
 )
 
@@ -420,5 +421,48 @@ func main() {
 
 	if string(formattedResult) != expectedOutput {
 		t.Errorf("got:\n%s\nwant:\n%s", formattedResult, expectedOutput)
+	}
+}
+
+func BenchmarkFormatSmall(b *testing.B) {
+	inputSource := []byte(`package main
+func main() {
+	x := 1
+	y := 2
+	if x > 0 {
+		z := 3
+	}
+	a := 4
+}
+`)
+	formatter := &Formatter{CommentMode: CommentsFollow}
+
+	for b.Loop() {
+		_, _ = formatter.Format(inputSource)
+	}
+}
+
+func BenchmarkFormatLarge(b *testing.B) {
+	var sourceBuilder strings.Builder
+
+	sourceBuilder.WriteString("package main\n\n")
+
+	for functionIndex := range 100 {
+		sourceBuilder.WriteString("func foo")
+		sourceBuilder.WriteString(string(rune('A' + functionIndex%26)))
+		sourceBuilder.WriteString("() {\n")
+		sourceBuilder.WriteString("\tx := 1\n")
+		sourceBuilder.WriteString("\tif x > 0 {\n")
+		sourceBuilder.WriteString("\t\ty := 2\n")
+		sourceBuilder.WriteString("\t}\n")
+		sourceBuilder.WriteString("\tz := 3\n")
+		sourceBuilder.WriteString("}\n\n")
+	}
+
+	inputSource := []byte(sourceBuilder.String())
+	formatter := &Formatter{CommentMode: CommentsFollow}
+
+	for b.Loop() {
+		_, _ = formatter.Format(inputSource)
 	}
 }
